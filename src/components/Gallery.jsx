ac0b8057c68b4788a84aac5716907e7751d232d2
Zahid375 <zahidislam375@gmail.com>
2023-11-05 02:17:19 +0600

    Gallery update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,27 +6,27 @@ function Gallery() {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [images, setImages] = useState([
     {
-      id: 1,
+      id: "image-1",
       url: '/images/image-1.webp',
       alt: 'Image 1 Alt Text',
     },
     {
-      id: 2,
+      id: "image-2",
       url: '/images/image-2.webp',
       alt: 'Image 2 Alt Text',
     },
     {
-      id: 3,
+      id: "image-3",
       url: '/images/image-3.webp',
       alt: 'Image 3 Alt Text',
     },
     {
-      id: 4,
+      id: "image-3",
       url: '/images/image-4.webp',
       alt: 'Image 4 Alt Text',
     },
     {
-      id: 5,
+      id: "image-5",
       url: '/images/image-5.webp',
       alt: 'Image 5 Alt Text',
     },
@@ -48,7 +48,8 @@ function Gallery() {
   const uncheckedall = () => {
     setSelectedFiles([]);
     images.forEach((image, index) => {
-      const checkbox = document.getElementById(index);
+      const checkbox = document.getElementById(image.id);
+      console.log(checkbox);
       if (checkbox) {
         checkbox.checked = false;
         checkbox.style.display = '';
@@ -64,14 +65,14 @@ function Gallery() {
     }
 
     const updatedImages = images.filter(
-      (image) => !selectedFiles.includes(image.id.toString())
+      (image) => !selectedFiles.includes(image.id)
     );
     setImages(updatedImages);
 
     // Clear the selectedFiles array
     setSelectedFiles([]);
     images.forEach((image, index) => {
-      const checkbox = document.getElementById(index);
+      const checkbox = document.getElementById(image.id);
       if (checkbox) {
         checkbox.checked = false;
         checkbox.style.display = '';
@@ -81,6 +82,7 @@ function Gallery() {
   };
 
   return (
+    
     <div className="container">
       <div className="gallery-header">
         <div>
@@ -123,13 +125,13 @@ function Gallery() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="upload-image"
+            className="upload-image"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
             />
           </svg>
