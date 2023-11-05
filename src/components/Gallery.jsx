a7ab128f3a2ce95f60f3780fe7b6ac2215fa8d92
Zahid375 <zahidislam375@gmail.com>
2023-11-06 03:47:26 +0600

    Dragable enable

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,36 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/style.css';
 import Image from './Image';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
+
+const data = [
+  {
+    id: "item-1",
+    url: '/images/image-1.webp',
+    alt: 'Image 1 Alt Text',
+  },
+  {
+    id: "item-2",
+    url: '/images/image-2.webp',
+    alt: 'Image 2 Alt Text',
+  },
+  {
+    id: "item-3",
+    url: '/images/image-3.webp',
+    alt: 'Image 3 Alt Text',
+  },
+  {
+    id: "item-4",
+    url: '/images/image-4.webp',
+    alt: 'Image 4 Alt Text',
+  },
+  {
+    id: "item-5",
+    url: '/images/image-5.webp',
+    alt: 'Image 5 Alt Text',
+  },
+];
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging ? "lightgreen" : "grey",
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+});
 function Gallery() {
   const [selectedFiles, setSelectedFiles] = useState([]);
-  const [images, setImages] = useState([
-    {
-      id: "image-1",
-      url: '/images/image-1.webp',
-      alt: 'Image 1 Alt Text',
-    },
-    {
-      id: "image-2",
-      url: '/images/image-2.webp',
-      alt: 'Image 2 Alt Text',
-    },
-    {
-      id: "image-3",
-      url: '/images/image-3.webp',
-      alt: 'Image 3 Alt Text',
-    },
-    {
-      id: "image-3",
-      url: '/images/image-4.webp',
-      alt: 'Image 4 Alt Text',
-    },
-    {
-      id: "image-5",
-      url: '/images/image-5.webp',
-      alt: 'Image 5 Alt Text',
-    },
-  ]);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(data);
+  },[]);
 
   const handleCheckboxChange = (event) => {
     const { id, checked } = event.target;
@@ -81,8 +111,23 @@ function Gallery() {
     });
   };
 
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const reorderedItems = reorder(
+      images,
+      result.source.index,
+      result.destination.index
+    );
+
+    console.log({ reorderedItems });
+    setImages(reorderedItems);
+  };
+
   return (
-    
+
     <div className="container">
       <div className="gallery-header">
         <div>
@@ -110,35 +155,32 @@ function Gallery() {
           )}
         </div>
       </div>
-      <div className="gallery-body">
-        {images.map((image, index) => (
-          <Image
-            key={index}
-            id={image.id}
-            src={image.url}
-            alt={image.alt}
-            onChange={handleCheckboxChange}
-          />
-        ))}
-        <div id="uplaod-area" className="add-image">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="upload-image"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-            />
-          </svg>
-          <input type="file" name="image" id="image-input" className="hidden" />
-          <p>Add Image</p>
-        </div>
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="gallery">
+          {(provided,snapshot) => (
+            <div
+              className="gallery-body"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {images.map((image, index) => (
+                <Image
+                  key={image.id}
+                  id={image.id}
+                  src={image.url}
+                  alt={image.alt}
+                  index={index}
+                  onChange={handleCheckboxChange}
+                  
+                />
+              ))}
+              {provided.placeholder}
+             
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import '../css/style.css';
 
-function Image({ src, alt, id, onChange }) {
+function Image({ src, alt, id, index, onChange }) {
   return (
-    <div className="image-item">
-      <input
-        type="checkbox"
-        className="image-selector"
-        id={id}
-        onChange={onChange}
-      />
-      <img src={src} alt={alt} />
-    </div>
+    <Draggable draggableId={id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className={`image-item ${snapshot.isDragging ? 'dragging' : ''}`}
+        >
+          <input
+            type="checkbox"
+            className="image-selector"
+            id={id}
+            onChange={onChange}
+          />
+          <img src={src} alt={alt} />
+        </div>
+      )}
+    </Draggable>
   );
 }
 
